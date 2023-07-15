8ad953e969371787002c5a807c901ce2a9aba49a
Robinzon <robinzon.m.g.f@gmail.com>
2023-07-14 20:08:04 -0400

    Agregando ultimos detalles

---
+++ js/boton-nocturno.js
@@ -0,0 +1,46 @@
+var miBotonFlotante = document.getElementById('boton_img');
+var urlNav = document.querySelectorAll('.menu-links');
+var textoPresentacion = document.querySelectorAll('.color_nocturno');
+var colorFondo = document.querySelectorAll('.background_nocturno');
+var colorRedesSociales = document.querySelectorAll('.redes_sociales-nocturno')
+var bodyColor = document.body;
+
+var claro = true;
+
+miBotonFlotante.addEventListener('click', function () {
+    if (claro) {
+        miBotonFlotante.style.backgroundImage = "url('../recursos/imagenes-iconos/sol.png')";
+
+        urlNav.forEach(function (urlNav) {
+            urlNav.classList.add("color_noche-links--nav");
+        });
+        textoPresentacion.forEach(function (textoPresentacion) {
+            textoPresentacion.classList.add("color_nocturno-activado");
+        });
+        colorFondo.forEach(function (colorFondo) {
+            colorFondo.classList.add("background_nocturno-activado");
+        });
+        colorRedesSociales.forEach(function (colorRedesSociales) {
+            colorRedesSociales.classList.add("redes_sociales-nocturno--activado");
+        });
+
+        claro = false;
+    } else {
+        miBotonFlotante.style.backgroundImage = "url('../recursos/imagenes-iconos/luna.png')";
+        
+        urlNav.forEach(function (urlNav) {
+            urlNav.classList.remove("color_noche-links--nav");
+        });
+        textoPresentacion.forEach(function (textoPresentacion) {
+            textoPresentacion.classList.remove("color_nocturno-activado");
+        });
+        colorFondo.forEach(function (colorFondo) {
+            colorFondo.classList.remove("background_nocturno-activado");
+        });
+        colorRedesSociales.forEach(function (colorRedesSociales) {
+            colorRedesSociales.classList.remove("redes_sociales-nocturno--activado");
+        });
+
+        claro = true;
+    }
+});
--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ import { valida } from "./funcionalidad.js";
 const inputs = document.querySelectorAll("input");
 
 inputs.forEach(input => {
-    input.addEventListener('blur', (input) =>{
+    input.addEventListener('blur', (input) => {
         valida(input.target);
     })
 })
 
-//! Para evitar el click derecho en la pagina
-document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-});
+// //! Para evitar el click derecho en la pagina
+// document.addEventListener('contextmenu', function(e) {
+//     e.preventDefault();
+// });
